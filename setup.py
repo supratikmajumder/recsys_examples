f34d288ce43dc71fba1d9cce6a1602cac4dadee3
Majumder <supratik.majumder@intel.com>
2023-03-27 12:23:30 +0530

    Initial commit for recommendation system models.
    Includes 4 python notebooks for different flavors of recommendation systems:
    1. Popularity based,
    2. Content based,
    3. Advanced content based, and
    4. Collaborative filtering (somewhat simplified NCF implementation).
    
    All the notebooks use the 100k subset from MovieLens dataset.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Simple Recommendataion System Tutorial for Movies',
+    author='Supratik Majumder',
+    license='MIT',
+)
